849c2629f8ca287d59dcb45440d8f3282703a9b8
Benjamin Dos Santos <hello@b-ds.fr>
2011-12-29 15:54:08 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,45 @@
+/*
+ * TODO:
+ * - Logging options (path, file, syslog ...)
+ * - Daemon config
+ */
+class vnstat($database_dir = '/home/vnstat', $interface = "eth0") {
+	
+	package { 
+		'vnstat':
+        ensure => 'installed'
+    }
+	
+	file {
+        $database_dir:
+		ensure => 'directory',
+        mode => 0700,
+        owner  => 'root',
+		group  => 'root';
+		
+		'/etc/vnstat.conf':
+		ensure => 'file',
+		mode => 0644,
+		content => template('vnstat/etc/vnstat.conf.erb'),
+        owner  => 'root',
+		group  => 'root',
+		before => Package['vnstat'],
+		notify => Service['vnstat'],
+	}
+	
+	service {
+		'vnstat':
+        ensure => 'running',
+		enable => true,
+        hasstatus => true,
+        hasrestart => true,
+	}
+	
+	exec { 
+		new-database:
+	  	command => "/usr/bin/vnstat -u -i ${interface}",
+	  	creates => "${$database_dir}/${$interface}",
+		require => Package['vnstat'],
+		notify => Service['vnstat'],
+	}
+}
